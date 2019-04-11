e8f6902683caeb812e3f2f481558efacddab2f43
John Snyder <me@johnsnyder.org>
2019-04-11 07:19:01 -0500

    Starting fresh in the morning!  new org and presentation updates

--- STL_RUG_Meetup/Data_Demo.R
+++ STL_RUG_Meetup/Data_Demo.R
@@ -35,24 +35,44 @@ rmse_by_num_trees(bart.model, num_replicates = 20)
 bart.model.cv <- bartMachineCV(X, y,
                                num_burn_in = 1000,
                                num_iterations_after_burn_in = 5000)
-  
+
+investigate_var_importance(bart.model.cv)
+interaction_investigator(bart.model.cv)
+
+VarSel <- var_selection_by_permute(bart.model.cv)
+VarSel$important_vars_local_names
+
+RegressMod <- lm(formula = medv ~ ., data = Boston)
+summary(RegressMod)
+
+pd_plot(bart.model.cv, j = "lstat")
+cov_importance_test(bart.model.cv, covariates = "lstat")
+
+pd_plot(bart.model.cv, j = "indus")
+cov_importance_test(bart.model.cv, covariates = "indus")
+
+cov_importance_test(bart.model.cv)
+
+## check your asusmptions
 check_bart_error_assumptions(bart.model.cv)  
 plot_convergence_diagnostics(bart.model.cv)  
 
 plot_y_vs_yhat(bart.model.cv, credible_intervals = TRUE)
 
-investigate_var_importance(bart.model.cv, num_replicates_for_avg = 20) 
-interaction_investigator(bart.model.cv, num_replicates_for_avg = 25,num_var_plot = 10, bottom_margin = 5)
 
-pd_plot(bart.model.cv, j = "lstat")
-pd_plot(bart.model.cv, j = "rm")
 
-cov_importance_test(bart.model.cv)
-cov_importance_test(bart.model.cv, covariates = "lstat")
+# predictions
+MeanDF <- colMeans(X)
+predict(bart.model.cv, newdata = MeanDF)
+calc_credible_intervals(bart.model.cv, MeanDF, ci_conf = 0.95) %>% round(digits=2)
+calc_prediction_intervals(bart.model.cv, MeanDF, pi_conf = 0.95) %>% round(digits=2)
 
-vs<-
-var_selection_by_permute(bart_machine,bottom_margin = 10, num_permute_samples = 10)
-vs$important_vars_local_names
+
+
+
+
+
+
 
 ###
 set.seed(1)
