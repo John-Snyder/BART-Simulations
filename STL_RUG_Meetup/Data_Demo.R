cdc6457e479215af8898541eeeb8a04686f45210
John Snyder <me@johnsnyder.org>
2019-04-10 22:02:19 -0500

    changes

--- STL_RUG_Meetup/Data_Demo.R
+++ STL_RUG_Meetup/Data_Demo.R
@@ -1,3 +1,4 @@
+library(MASS)
 library(tidyverse)
 
 options(java.parameters = "-Xmx10g")
@@ -9,4 +10,81 @@ library(xgboost)
 library(randomForest)
 library(caret)
 
-data(benchmark_datasets)
+
+data(Boston)
+
+Boston %>% head %>% View
+
+RegressMod <- lm(formula = medv ~ ., data = Boston)
+summary(RegressMod)
+
+set.seed(1)
+train <- sample(1:nrow(Boston), .8*nrow(Boston))
+
+y.train <- Boston$medv[train]
+y.test <- Boston$medv[-train]
+
+X.train <- Boston %>% dplyr::select(-c("medv")) %>% dplyr::slice(train)
+X.test <- Boston %>% dplyr::select(-c("medv")) %>% dplyr::slice(-train)
+
+#Fit BART model
+bart.model <- bartMachine(X.train,y.train,
+                          num_trees = 200,
+                          num_burn_in = 100,
+                          num_iterations_after_burn_in = 100)
+
+
+bart.model
+  
+k_fold_cv(X.train, y.train, k_folds = 10)
+
+rmse_by_num_trees(bart.model, num_replicates = 20)  
+
+bart.model.cv <- bartMachineCV(X.train, y.train,
+                               num_burn_in = 100,
+                               num_iterations_after_burn_in = 100)
+  
+check_bart_error_assumptions(bart.model.cv)  
+plot_convergence_diagnostics(bart.model.cv)  
+
+plot_y_vs_yhat(bart.model.cv, credible_intervals = TRUE)
+
+investigate_var_importance(bart.model.cv, num_replicates_for_avg = 20) 
+interaction_investigator(bart.model.cv, num_replicates_for_avg = 25,
+                         + num_var_plot = 10, bottom_margin = 5)
+
+pd_plot(bart.model.cv, j = "lstat")
+pd_plot(bart.model.cv, j = "rm")
+
+
+###
+nsim <- 1000
+x1 <- runif(nsim,-3.14,3.14)
+x2 <- runif(nsim,-3.14,3.14)
+
+y <- sin(x2)
+plot(x2,y)
+
+y <- x1+sin(x2) + rnorm(nsim)
+
+bart.model <- bartMachine(data.frame(x1,x2),y,
+                          num_trees = 200,
+                          num_burn_in = 1000,
+                          num_iterations_after_burn_in = 2000)
+
+pd_plot(bart.model, j = "x2")
+###
+
+cov_importance_test(bart.model.cv)
+
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+    
